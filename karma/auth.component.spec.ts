5149591f65b60bad144df52d941a23bb9b1f20d7
Gowthambalaji825 <18euec056@skcet.ac.in>
2022-03-22 05:27:25 +0000

    Final Commit

---
+++ karma/auth.component.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { AuthComponent } from './auth.component';
+
+describe('AuthComponent', () => {
+  let component: AuthComponent;
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule], 
+    providers: [AuthComponent]
+  }));
+  beforeEach(() => {
+    const fixture = TestBed.createComponent(AuthComponent);
+    component = fixture.componentInstance;
+  });
+  it('FE_AuthTest', () => {
+    expect(component).toBeTruthy();
+  });
+});
