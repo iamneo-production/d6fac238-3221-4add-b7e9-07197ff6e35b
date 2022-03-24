43eca3de2a516ecb1bc8aa7d1c9b5f73d1efc33f
arun-cr-09 <18tucs015@skct.edu.in>
2022-03-24 07:08:20 +0000

    user-backend

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,13 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,LoginComponent,
-    SignupComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
