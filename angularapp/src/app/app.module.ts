3da8d4efd8822d20efda5b96dbce24742fb09eab
arun-cr-09 <18tucs015@skct.edu.in>
2022-03-24 02:05:23 +0000

    angualr-module changes

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent,LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
