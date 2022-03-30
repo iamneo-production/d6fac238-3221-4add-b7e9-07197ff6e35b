3799f10b9d2afb1a2a6b27862615eca3e3df78a9
dhanujashree2000 <dhanujashree2000@gmail.com>
2022-03-30 09:07:05 +0000

    Login modified

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    SignupComponent,
+    NavbarComponent,
+    AuthComponent,
+    LoginComponent,
+    
+ 
+    
   ],
   imports: [
     BrowserModule,
