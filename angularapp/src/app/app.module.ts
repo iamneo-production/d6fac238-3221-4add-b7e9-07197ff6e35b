efc9c657d3cb1ecc7c88ee0b782ab7956578d2d3
arun-cr-09 <18tucs015@skct.edu.in>
2022-04-11 05:09:51 +0000

    changes done

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,37 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 
 @NgModule({
   declarations: [
     AppComponent
-=======
->>>>>>> 131bdfba65346034eed2d65f32748cd10e8ebd6a
-import { AuthComponent } from './auth/auth.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    AuthComponent,
-    LoginComponent,
-    SignupComponent
-<<<<<<< HEAD
-=======
->>>>>>> BarkNBurr
->>>>>>> 131bdfba65346034eed2d65f32748cd10e8ebd6a
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
