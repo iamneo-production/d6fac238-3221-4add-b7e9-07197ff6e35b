b8aa0eca3dac03e0894841adf00d734b8773442b
Divya1-34 <durai743@gmail.com>
2022-04-19 05:28:25 +0000

    commited

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -2,21 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< HEAD
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-=======
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
->>>>>>> 0e97bace84b3a9333084ddd43328595e6bf8045c
   ],
   imports: [
     BrowserModule,
